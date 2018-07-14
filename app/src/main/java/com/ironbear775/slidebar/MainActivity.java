15706ea4d253e3375e7459db84b319210085ab6e
ironbear775 <ironbear775@gmail.com>
2018-07-14 20:46:04 +0800

    update

--- app/src/main/java/com/ironbear775/slidebar/MainActivity.java
+++ app/src/main/java/com/ironbear775/slidebar/MainActivity.java
@@ -1,14 +1,12 @@
 package com.ironbear775.slidebar;
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 /**
  * @author ironbear775
  */
 public class MainActivity extends Activity {
-    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +19,6 @@ public class MainActivity extends Activity {
             @Override
             public void onTouchListener(String letter,int position) {
                 textView.setText(letter);
-                Log.e(TAG, "position: "+position);
             }
         });
 
